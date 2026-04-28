b91b47ed18cb787afd2ccfe494bdf15b19e9510d
pb <xichanzheng@gmail.com>
2026-04-28 01:52:09 -0700

    Hiding Spot Function

---
+++ gdim33milestone1/Assets/Scripts/HidingSpot.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using TMPro;
+
+public class HidingSpot : MonoBehaviour
+{
+    public GameObject player;
+    public Transform hidePosition;
+    public Transform exitPosition;
+    public TextMeshProUGUI interactText;
+
+    private bool playerNearby = false;
+    private bool isHiding = false;
+
+    void Start()
+    {
+        interactText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            if (isHiding)
+                ExitHide();
+            else
+                EnterHide();
+        }
+    }
+
+    void EnterHide()
+    {
+        isHiding = true;
+        MovePlayerTo(hidePosition);
+        interactText.text = "Press E to leave hiding spot";
+    }
+
+    void ExitHide()
+    {
+        isHiding = false;
+        MovePlayerTo(exitPosition);
+        interactText.text = "Press E to enter hiding spot";
+    }
+
+    void MovePlayerTo(Transform target)
+    {
+        CharacterController cc = player.GetComponent<CharacterController>();
+
+        if (cc != null)
+        {
+            cc.enabled = false;
+        }
+
+        player.transform.position = target.position;
+        player.transform.rotation = target.rotation;
+
+        if (cc != null)
+        {
+            cc.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+            interactText.gameObject.SetActive(true);
+
+            if (isHiding)
+                interactText.text = "Press E to leave hiding spot";
+            else
+                interactText.text = "Press E to enter hiding spot";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && !isHiding)
+        {
+            playerNearby = false;
+            interactText.gameObject.SetActive(false);
+        }
+    }
+}
+
+
+
