389227e46e95a4a54724325905f7d34bbe70ce5e
pb <xichanzheng@gmail.com>
2026-05-25 20:23:28 -0700

    MouseLook Fixes

--- gdim33milestone1/Assets/Scripts/MouseLook.cs
+++ gdim33milestone1/Assets/Scripts/MouseLook.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 40f;
+    public float mouseSensitivity = 15f;
     public Transform playerBody;
 
     private float xRotation = 0f;
+    private float yRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +24,16 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        mouseX = Mathf.Clamp(mouseX,-2f,2f);
+        mouseY = Mathf.Clamp(mouseY,-2f,2f);
+
+        yRotation += mouseX;
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation,-80f,80f);
 
         transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
 
-        playerBody.Rotate(Vector3.up * mouseX);
+        playerBody.rotation = Quaternion.Euler(0f,yRotation,0f);
         
     }
 }
