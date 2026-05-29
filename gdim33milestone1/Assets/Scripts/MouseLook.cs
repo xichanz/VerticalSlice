47c859896ed91a619b00fbbd766dcc963c3fb683
xichan <xichanz@uci.edu>
2026-05-28 23:26:12 -0700

    Build02

--- gdim33milestone1/Assets/Scripts/MouseLook.cs
+++ gdim33milestone1/Assets/Scripts/MouseLook.cs
@@ -4,36 +4,30 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-
     public float mouseSensitivity = 15f;
     public Transform playerBody;
 
     private float xRotation = 0f;
     private float yRotation = 0f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
+#if UNITY_WEBGL
+        mouseSensitivity = mouseSensitivity / 2.0f;
+#endif
     }
 
-    // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        mouseX = Mathf.Clamp(mouseX,-2f,2f);
-        mouseY = Mathf.Clamp(mouseY,-2f,2f);
-
         yRotation += mouseX;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation,-80f,80f);
 
-        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
 
-        playerBody.rotation = Quaternion.Euler(0f,yRotation,0f);
-        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.rotation = Quaternion.Euler(0f, yRotation, 0f);
     }
 }
